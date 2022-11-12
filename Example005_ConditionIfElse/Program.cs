f6ecd3b919ee9b643a647a6b1c884966e291d638
Dmitriy <dtinukov@gmail.com>
2022-11-12 13:00:29 +0300

    add Example005 применение if Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя:") ;
+string username = Console.ReadLine();
+
+if (username == "Аня")
+{
+    Console.Write("Ура, это же Фешечка!!!");
+}
+else 
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
